d4691980495adc1cd8192dbe7f031a0ddf9d1046
Dan Danilyuk and Mark Helt <mark@>
2017-11-01 13:42:38 -0700

    write html for project cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,36 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { routing } from './app.routing';
 
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { Router } from '@angular/router';
+import { ProjectService } from '../project.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ProjectService]
 })
 export class HomeComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
+  }
+
+  timeLeft(project) {
+    let diff = Math.floor(((project.startDate - project.goalDate)/1000)/86400)
+    return diff;
+  }
+
+  shortDescription(project) {
+    let short;
+    if (project.name.length + project.description.length > 80) {
+      short = project.description.substring(0,80-(project.name.length)) + "..."
+    }
+    else {
+      short =  project.description;
+    }
+    return short;
   }
 
 }
